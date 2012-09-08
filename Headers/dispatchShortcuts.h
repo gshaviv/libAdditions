7ff63ee44ace1b9768807ecf43946a8e03e99fdd
Guy Shaviv <guy@shaviv.org>
2012-09-08 11:43:11 +0300

    dispatch shortcuts

--- Headers/dispatchShortcuts.h
+++ Headers/dispatchShortcuts.h
@@ -18,13 +18,16 @@ static inline void dispatch_sync_main(dispatch_block_t block) {
 	dispatch_sync(dispatch_get_main_queue(),block);
 }
 typedef enum {
-	PRIORITY_HIGH = 2,
-	PRIORITY_DEFAULT = 0,
-	PRIORITY_LOW = -2
+	PRIORITY_HIGH = DISPATCH_QUEUE_PRIORITY_HIGH,
+	PRIORITY_DEFAULT = DISPATCH_QUEUE_PRIORITY_DEFAULT,
+	PRIORITY_LOW = DISPATCH_QUEUE_PRIORITY_LOW
 } queue_priority_t;
 
 static inline void dispatch_async_priority(queue_priority_t priority, dispatch_block_t block) {
 	dispatch_async(dispatch_get_global_queue(priority,0),block);
+}
+static inline void dispatch_async_default_priority(dispatch_block_t block) {
+	dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT,0),block);
 }
 static inline void dispatch_sync_priority(queue_priority_t priority, dispatch_block_t block) {
 	dispatch_sync(dispatch_get_global_queue(priority,0),block);
@@ -44,6 +47,24 @@ static inline void if_needed_dispatch_async_main(dispatch_block_t block) {
         dispatch_async(dispatch_get_main_queue(),block);
     }
 }
+
+static inline void if_needed_dispatch_async(dispatch_queue_t queue, dispatch_block_t block) {
+    if (dispatch_get_current_queue() == queue) {
+        block();
+    } else {
+        dispatch_async(queue,block);
+    }
+}
+
+static inline void if_needed_dispatch_sync(dispatch_queue_t queue, dispatch_block_t block) {
+    if (dispatch_get_current_queue() == queue) {
+        block();
+    } else {
+        dispatch_sync(queue,block);
+    }
+}
+
+
 
 
 #endif
